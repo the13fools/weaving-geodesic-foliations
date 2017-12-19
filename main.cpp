08c78dc1869f1e2fc93f45c8d732c2e09aed7e59
Etienne Vouga <evouga@gmail.com>
2017-12-18 23:28:23 -0600

    Here's how to turn on and modify the GUI

--- main.cpp
+++ main.cpp
@@ -136,86 +136,111 @@ void computeRecoveredDistanceField_test(const Eigen::MatrixXd &W_local,
     }
 }
 
-int main(int argc, char *argv[])
+Eigen::MatrixXd V;
+Eigen::MatrixXi F, E, F_edges;
+
+igl::viewer::Viewer *viewer;
+
+double px = 0;
+double py = 0;
+
+void showVectorField()
 {
+    Eigen::MatrixXd colorField;
+    Eigen::MatrixXd centroids_F;
+    computeCentroids(F,V,centroids_F);
 
-  Eigen::MatrixXd V;
-  Eigen::MatrixXi F, E, F_edges;
+    Eigen::Vector3d p(px, py,0);
+    Eigen::MatrixXd W;
+    computeDistanceField(p, centroids_F, W);
+    //  computeWhirlpool(p, centroids_F, W);
 
-  Eigen::MatrixXd colorField;
+
+    Eigen::MatrixXd W_test;
+    computeDistanceField(p, centroids_F, W_test);
+    //  computeTestField(p, centroids_F, W_test);
+
+    Eigen::MatrixXd W_local;
+    computeLocalCoordinatesForDistanceField(W_test, F, V, W_local);
+
+    Eigen::MatrixXd del_W_F;
+    del_W_F.resize(F.rows(), 3);
+    del_W_F.setZero();
+    Eigen::VectorXd scalar_E;
+    for (int i = 0; i < 3; i++) 
+    {
+        computeEdgeWeights(W.col(i), V, E, scalar_E); 
+        computeCovariantDerivative(W_local, F, F_edges, V, scalar_E, del_W_F, i);
+    }
+    //  std::cout << del_W_F;
+    //  Eigen::MatrixXd W_recovered;
+    //  computeRecoveredDistanceField_test(W_local, F, V, W_recovered);
+
+    // Average edge length for sizing
+    const double avg = igl::avg_edge_length(V,F);
+
+    int nFaces = F.rows(); 
+    colorField.resize(nFaces, 3);
+
+    Eigen::VectorXd Z(nFaces);
+    double maxerror = 0;
+    for (int i = 0; i < nFaces; i++)
+    {
+        Z(i) = log(del_W_F.row(i).norm());
+        if (maxerror < Z(i))
+        {
+            maxerror = Z(i);
+            std::cout << del_W_F.row(i) << "\n";
+        }
+    }
+    //  Eigen::VectorXd Z = W.col(0); // - del_W_F;// - W_recovered.col(0);
+    // Eigen::VectorXd Z = del_W_F.transpose() * del_W_F;// - W_recovered.col(0);
+
+    //  igl::jet(Z,true,colorField);
+    igl::colormap(igl::COLOR_MAP_TYPE_MAGMA,Z, true, colorField);
+
+
+    // Plot the mesh
+    viewer->data.clear();
+    viewer->data.set_mesh(V, F);
+    viewer->data.set_face_based(true);
+
+    viewer->data.set_colors(colorField);
+
+    Eigen::MatrixXd eps = Eigen::MatrixXd::Constant(nFaces,3,.001);
+
+    const Eigen::RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
+    viewer->data.add_edges(centroids_F  + del_W_F*avg/2, centroids_F, blue);
+}
+
+int main(int argc, char *argv[])
+{  
   //   assignFaceVal(F,viz);;
 
   igl::readOBJ("../circ.obj", V, F);
   buildEdges(F, E);
   buildEdgesPerFace(F, E, F_edges);
 
-  Eigen::MatrixXd centroids_F;
-  computeCentroids(F,V,centroids_F);
-  
-  Eigen::Vector3d p(.0,.0,0);
-  Eigen::MatrixXd W;
-  computeDistanceField(p, centroids_F, W);
-//  computeWhirlpool(p, centroids_F, W);
-
-
-  Eigen::MatrixXd W_test;
-  computeDistanceField(p, centroids_F, W_test);
-//  computeTestField(p, centroids_F, W_test);
-
-  Eigen::MatrixXd W_local;
-  computeLocalCoordinatesForDistanceField(W_test, F, V, W_local);
-
-  Eigen::MatrixXd del_W_F;
-  del_W_F.resize(F.rows(), 3);
-  del_W_F.setZero();
-  Eigen::VectorXd scalar_E;
-  for (int i = 0; i < 3; i++) 
+  // Plot the mesh  
+  viewer = new igl::viewer::Viewer();
+  viewer->data.set_mesh(V, F);
+  viewer->data.set_face_based(true);
+  viewer->callback_init = [&](igl::viewer::Viewer& viewer)
   {
-      computeEdgeWeights(W.col(i), V, E, scalar_E); 
-      computeCovariantDerivative(W_local, F, F_edges, V, scalar_E, del_W_F, i);
-  }
-//  std::cout << del_W_F;
-//  Eigen::MatrixXd W_recovered;
-//  computeRecoveredDistanceField_test(W_local, F, V, W_recovered);
+      // Add new group
+      viewer.ngui->addGroup("Vector Field Options");
 
-  // Average edge length for sizing
-  const double avg = igl::avg_edge_length(V,F);
-  
-  int nFaces = F.rows(); 
-  colorField.resize(nFaces, 3);
+      // Expose a variable
+      viewer.ngui->addVariable("Center X",px);
+      viewer.ngui->addVariable("Center Y",py);
 
-  Eigen::VectorXd Z(nFaces);
-  double maxerror = 0;
-  for (int i = 0; i < nFaces; i++)
-  {
-      Z(i) = log(del_W_F.row(i).norm());
-      if (maxerror < Z(i))
-      {
-	  maxerror = Z(i);
-          std::cout << del_W_F.row(i) << "\n";
-      }
-  }
-//  Eigen::VectorXd Z = W.col(0); // - del_W_F;// - W_recovered.col(0);
- // Eigen::VectorXd Z = del_W_F.transpose() * del_W_F;// - W_recovered.col(0);
-  
-//  igl::jet(Z,true,colorField);
-  igl::colormap(igl::COLOR_MAP_TYPE_MAGMA,Z, true, colorField);
+      // Add a button
+      viewer.ngui->addButton("Recompute Derivative", showVectorField);
 
+      // call to generate menu
+      viewer.screen->performLayout();
+      return false;
+  };
 
-  // Plot the mesh
-  igl::viewer::Viewer viewer;
-  viewer.data.set_mesh(V, F);
-  viewer.data.set_face_based(true);
- 
-  viewer.data.set_colors(colorField);
-
-  Eigen::MatrixXd eps = Eigen::MatrixXd::Constant(nFaces,3,.001);
-
-  const Eigen::RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
-  viewer.data.add_edges(centroids_F  + del_W_F*avg/2, centroids_F, blue);
-  //viewer.data.add_edges(centroids_F + W*avg/2 + eps, centroids_F + eps, red);
-//  viewer.data.add_edges(centroids_F + W*avg/2, centroids_F + eps, blue);
-//  viewer.data.add_edges(centroids_F, centroids_F *2 * avg, blue);
-
-  viewer.launch();
+  viewer->launch();
 }
