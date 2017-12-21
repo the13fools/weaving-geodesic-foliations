d6269a0eb92863e96f54c4d570e2ee2d811c81fd
Josh Vekhter <josh@foolishproducts.com>
2017-12-21 01:43:05 -0500

    moar refactoring

--- main.cpp
+++ main.cpp
@@ -62,26 +62,60 @@ void evaluateOperator(const Eigen::MatrixXd &DM_local,
     }
 }
 
+Eigen::MatrixXd W; // This can be thought of as 3 ``independent'' scalar fields
+Eigen::MatrixXd W_test; // The derivative is tested in this direction
+
+
+Eigen::MatrixXd colorField;
+Eigen::MatrixXd centroids_F;
+
+Eigen::MatrixXd del_W_F;
+
+
+void updateView(Eigen::VectorXd faceColors)
+{
+    // Average edge length for sizing
+    const double avg = igl::avg_edge_length(V,F);
+    int nFaces = F.rows(); 
+    colorField.resize(nFaces, 3);
+    
+    //  igl::jet(Z,true,colorField);
+    igl::colormap(igl::COLOR_MAP_TYPE_MAGMA,faceColors, true, colorField);
+
+
+    // Plot the mesh
+    viewer->data.clear();
+    viewer->data.set_mesh(V, F);
+    viewer->data.set_face_based(true);
+
+    viewer->data.set_colors(colorField);
+
+    Eigen::MatrixXd eps = Eigen::MatrixXd::Constant(nFaces,3,.001);
+
+    const Eigen::RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
+    viewer->data.add_edges(centroids_F  + del_W_F*avg/2, centroids_F, blue);
+}
+
+void takeGradientDescentStep()
+{
+
+}
+
+
 void showVectorField()
 {
-    Eigen::MatrixXd colorField;
-    Eigen::MatrixXd centroids_F;
     computeCentroids(F,V,centroids_F);
 
     Eigen::Vector3d p(px, py,0);
-    Eigen::MatrixXd W;
     computeDistanceField(p, centroids_F, W);
 //    computeWhirlpool(p, centroids_F, W);
 
-
-    Eigen::MatrixXd W_test;
     computeDistanceField(p, centroids_F, W_test);
 //    computeTestField(p, centroids_F, W_test);
 
     Eigen::MatrixXd W_local;
     computeLocalCoordinatesForDistanceField(W_test, F, V, W_local);
 
-    Eigen::MatrixXd del_W_F;
     del_W_F.resize(F.rows(), 3);
     del_W_F.setZero();
     Eigen::MatrixXd DM_local;
@@ -91,11 +125,7 @@ void showVectorField()
         evaluateOperator(DM_local, W_local, del_W_F, i);
     }
 
-    // Average edge length for sizing
-    const double avg = igl::avg_edge_length(V,F);
-
     int nFaces = F.rows(); 
-    colorField.resize(nFaces, 3);
 
     std::ofstream myfile ("derivatives.txt");
 
@@ -121,22 +151,11 @@ void showVectorField()
     //  Eigen::VectorXd Z = W.col(0); // - del_W_F;// - W_recovered.col(0);
     // Eigen::VectorXd Z = del_W_F.transpose() * del_W_F;// - W_recovered.col(0);
 
-    //  igl::jet(Z,true,colorField);
-    igl::colormap(igl::COLOR_MAP_TYPE_MAGMA,Z, true, colorField);
+    updateView(Z);   
 
-
-    // Plot the mesh
-    viewer->data.clear();
-    viewer->data.set_mesh(V, F);
-    viewer->data.set_face_based(true);
-
-    viewer->data.set_colors(colorField);
-
-    Eigen::MatrixXd eps = Eigen::MatrixXd::Constant(nFaces,3,.001);
-
-    const Eigen::RowVector3d red(0.8,0.2,0.2),blue(0.2,0.2,0.8);
-    viewer->data.add_edges(centroids_F  + del_W_F*avg/2, centroids_F, blue);
 }
+
+
 
 int main(int argc, char *argv[])
 {  
@@ -161,6 +180,7 @@ int main(int argc, char *argv[])
 
       // Add a button
       viewer.ngui->addButton("Recompute Derivative", showVectorField);
+      viewer.ngui->addButton("Grad Descent Step", takeGradientDescentStep);
 
       // call to generate menu
       viewer.screen->performLayout();
